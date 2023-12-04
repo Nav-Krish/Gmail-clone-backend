9013e250fe74eedc7708240ac2460f2692314b1e
Nav-Krish <navanethakrishnan11@gmail.com>
2023-12-04 18:45:29 +0530

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const app = express()
 require('dotenv').config()
-const cors=require('cors')
+const cors = require('cors')
 const authenticateUser=require('./middleware/authentication')
 
 const connectDB=require('./db/connectDB')
